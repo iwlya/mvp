977819dc3d8733c1168ae75fa838eb26fa55ddf6
Качкинов Эзен 8И22 <kachkinove@gmail.com>
2023-07-22 10:21:21 +0700

    Pt:1_10:full task

---
+++ 1.2.py
@@ -0,0 +1,5 @@
+f = str(input("Введите фамилию \n"))
+i = str(input("Введите имя \n"))
+fi = f + " " + i
+print(fi, (len(f) + len(i)))
+
---
+++ 1.5.py
@@ -0,0 +1,17 @@
+def f():
+    n = int(input())
+    if n == 3:
+        print("Команда выиграла")
+        return f(n)
+    elif n == 0:
+        print("Команда проиграла")
+        return f(n)
+    elif n == 1:
+        print("Команда сыграла в ничью")
+        return f(n)
+    else:
+        print("Нет такого результата")
+        return f(n)
+
+
+f()
