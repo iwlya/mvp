77f252415557348aae4bc656e257d0bd838c49f5
iwlya <138237846+iwlya@users.noreply.github.com>
2023-07-25 19:01:32 +0700

    Update 2.2.py

--- 2.2.py
+++ 2.2.py
@@ -1,6 +1,6 @@
 array = ["СТС", "ТНТ", "РОССИЯ", "ПЯТНИЦА"]
 print(array)
-s = str(input("Введите название канала:\n"))
-poz = int(input("Введите позицию вашего канала:\n"))
+s = str(input("Введите название канала: "))
+poz = int(input("Введите позицию вашего канала: "))
 array.insert(poz - 1, s)
 print(array)
